e6f9626234b34eb057f8fa8e92aaf51603817a15
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2021-01-18 17:27:49 +0100

    Bump version: 0.6.0 → 0.6.1

--- bitmex_async_rest/__init__.py
+++ bitmex_async_rest/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Anders Ellenshøj Andersen"""
 __email__ = 'andersa@atlab.dk'
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 from .api import BitMEXRestApi
 from asks.errors import BadStatus
